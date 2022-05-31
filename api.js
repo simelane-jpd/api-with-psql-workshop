879f6a14181dd6b9d10f5e9aea481499aebf5134
simelane-jpd <201575227@student.uj.ac.za>
2022-05-31 12:00:29 +0200

    api change

--- api.js
+++ api.js
@@ -8,29 +8,38 @@ module.exports = function (app, db) {
 
 	app.get('/api/garments', async function (req, res) {
 
-		const { gender, season } = req.query;
-		let garments =[];
-		const thegarments =
-		!gender && !season
-		  ? ''
-		  : `where ${gender ? `gender='${gender}'` : ''} ${
-			  gender && season ? 'and' : ''
-			} ${season ? `season='${season}'` : ''}`;
-  
-	  garments = await db.any(`select * from garment ${thegarments}`);
-		// add some sql queries that filter on gender & season
-		//if (!gender && !season) {
+
+		try { /* your current code here */
+			const { gender, season } = req.query;
+			let garments = [];
+			const thegarments =
+				!gender && !season
+					? ''
+					: `where ${gender ? `gender='${gender}'` : ''} ${gender && season ? 'and' : ''
+					} ${season ? `season='${season}'` : ''}`;
+
+			garments = await db.any(`select * from garment ${thegarments}`);
+			// add some sql queries that filter on gender & season
+			//if (!gender && !season) {
 			//garments = await db.many(`select * from garment`);
-		//}
-		//else if (gender && !season){
+			//}
+			//else if (gender && !season){
 			//garments = await db.many(`select * from garment where gender = $1`, [gender]);
-		//}
-		//else if (!gender && season){
+			//}
+			//else if (!gender && season){
 			//garments = await db.many(`select * from garment where season = $1`, [season]);
-		//}
-		res.json({
-			data: garments
-		})
+			//}
+			res.json({
+				data: garments
+			})
+
+		}
+		
+		 catch (err) { 
+			 console.log(err);
+			res.json({ error: err.message }) }
+
+
 	});
 
 	app.put('/api/garment/:id', async function (req, res) {
@@ -40,30 +49,30 @@ module.exports = function (app, db) {
 			// use an update query...
 
 			const { id } = req.params;
-			const garmentinfo = {...req.body};
-		const fields = Object.keys(garmentinfo);
-		let newgarments = [];
-		for (const entry in garmentinfo) {
-		  if (fields.includes(entry)) {
-			newgarments = [...newgarments, `${entry} = '${garmentinfo[entry]}'`];
-		  }
-		}
-		await db.none(`update garment set ${newgarments.toString()} where id=$1`, id);
-			
-		
+			const garmentinfo = { ...req.body };
+			const fields = Object.keys(garmentinfo);
+			let newgarments = [];
+			for (const entry in garmentinfo) {
+				if (fields.includes(entry)) {
+					newgarments = [...newgarments, `${entry} = '${garmentinfo[entry]}'`];
+				}
+			}
+			await db.none(`update garment set ${newgarments.toString()} where id=$1`, id);
+
+
 			//const garment = await db.oneOrNone(`select * from garment where id = $1`, [id]);
 			//'select gender from garment where id = $1'
 
 			//'update garment set gender = $1 where gender = $2 and id = $3'
 			//let params = { ...garment, ...req.body };
 			//const { description, price, img, season, gender } = params;
-			 //const garment = await db.oneOrNone(`select * from garment where id = $1`, [id]);
+			//const garment = await db.oneOrNone(`select * from garment where id = $1`, [id]);
 
 			// you could use code like this if you want to update on any column in the table
 			// and allow users to only specify the fields to update
-			
-			 //let params = { ...garment, ...req.body };
-			 //const { description, price, img, season, gender } = params;
+
+			//let params = { ...garment, ...req.body };
+			//const { description, price, img, season, gender } = params;
 
 
 			res.json({
@@ -112,7 +121,7 @@ module.exports = function (app, db) {
 			await db.none(
 				'insert into garment (description, price, img, season, gender) values ($1,$2,$3,$4,$5)',
 				[description, price, img, season, gender],
-			  );
+			);
 			//await db.none(`insert into garment ${description},${price},${img},${season},${gender}`)
 
 			res.json({
@@ -131,7 +140,7 @@ module.exports = function (app, db) {
 	app.get('/api/garments/grouped', async function (req, res) {
 		const result = await db.many(
 			`select gender, count(*) from garment group by gender order by count(*) asc`,
-		  );
+		);
 		//const result = []		
 		// use group by query with order by asc on count(*)
 		res.json({
@@ -153,7 +162,7 @@ module.exports = function (app, db) {
 			// console.log(err);
 			res.json({
 				status: 'success',
-				error : err.stack
+				error: err.stack
 			})
 		}
 	});
